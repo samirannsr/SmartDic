3055758fa9ee3cc51ecc3b6e9e656481cb237695
samira <samira.nasiri777@gmail.com>
2023-01-19 19:02:02 +0100

    Initial Commit

---
+++ SmartDic/SmartDicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartDicApp.swift
+//  SmartDic
+//
+//  Created by Samira Nasiri on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct SmartDicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
